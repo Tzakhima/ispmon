09861ce18efdc55ae1e7aacdeea55598bf1a92f3
Tzakhi Malka <tzakhi.m@tzakhi-ma-nmbp.office.taboola.com>
2021-01-16 23:30:14 +0200

    Adding GO client support

--- server/export/export.py
+++ server/export/export.py
@@ -10,6 +10,7 @@ app.config["DEBUG"] = True
 
 # Redis
 message_r = redis.Redis(host='localhost', port=6379, db=0)
+message_g = redis.Redis(host='localhost', port=6379, db=1)
 
 
 @app.route('/metrics', methods=['get'])
@@ -47,6 +48,41 @@ ispmon:speed_download{ unique_id= "{{ metric['unique_id'] }}", country="{{ metri
     {%- if metric['speed']['results']['upload'] is number %}
 ispmon:speed_upload{ unique_id= "{{ metric['unique_id'] }}", country="{{ metric['country'] }}", isp= "{{ metric['isp'] }}" } {{ metric['speed']['results']['upload'] }}
     {%- endif %}
+    {%- endif %}
+    {%- endfor %}
+    '''
+    metric_template = Template(template)
+    return metric_template.render(metrics=d_data)
+
+@app.route('/gometrics', methods=['get'])
+def get_gometrics():
+    data = message_g.lrange('metrics', 0, -1)
+    message_g.delete('metrics')
+    # decode
+    d_data = []
+    for x in data:
+        d_data.append(pickle.loads(x))
+    print(d_data)
+    template = '''
+    {%- for metric in metrics %}
+    {%- for destination, data in metric['Ping'].items() %}
+    {%- if data['packetLoss'] is number %}
+ispmon:go:pacekt_loss_percent{ unique_id= "{{ metric['UID'] }}", country="{{ metric['Country'] }}", isp= "{{ metric['Isp'] }}", destination = "{{  destination  }}" }  {{ data['packetLoss'] }}
+    {%- endif %}
+    {%- if data['avgRTT'] is number %}
+ispmon:go:rtt_avg{ unique_id= "{{ metric['UID'] }}", country="{{ metric['Country'] }}", isp= "{{ metric['Isp'] }}", destination = "{{  destination  }}" }  {{ data['avgRTT'] }}
+    {%- endif %}
+    {%- endfor %}
+{%- for http_object in metric['HTTP'] %}
+{%- for destination, data in http_object.items() %}
+ispmon:go:http_conntime{ unique_id= "{{ metric['UID'] }}", country="{{ metric['Country'] }}", isp= "{{ metric['Isp'] }}", destination = "{{  destination  }}" }  {{ data['connTime'] }}
+ispmon:go:http_dnstime{ unique_id= "{{ metric['UID'] }}", country="{{ metric['Country'] }}", isp= "{{ metric['Isp'] }}", destination = "{{  destination  }}" }  {{ data['dnsTime'] }}
+ispmon:go:http_tlstime{ unique_id= "{{ metric['UID'] }}", country="{{ metric['Country'] }}", isp= "{{ metric['Isp'] }}", destination = "{{  destination  }}" }  {{ data['tlsTime'] }}
+ispmon:go:http_ttfbtime{ unique_id= "{{ metric['UID'] }}", country="{{ metric['Country'] }}", isp= "{{ metric['Isp'] }}", destination = "{{  destination  }}" }  {{ data['ttfbTime'] }}
+    {%- endfor %}
+    {%- endfor %}
+    {%- if metric['Speed'] != "null" %}
+ispmon:go:speed_download{ unique_id= "{{ metric['UID'] }}", country="{{ metric['Country'] }}", isp= "{{ metric['Isp'] }}" } {{ metric['Speed'] }}
     {%- endif %}
     {%- endfor %}
     '''
